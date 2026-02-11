e78dd4ddac84dd433607a3e5dd60f602077cd316
Coda <jbenson26@orono.k12.mn.us>
2026-02-11 17:54:22 -0600

    Update DriveCommands.java

--- src/main/java/frc/robot/commands/DriveCommands.java
+++ src/main/java/frc/robot/commands/DriveCommands.java
@@ -33,8 +33,8 @@ import java.util.function.Supplier;
 
 public class DriveCommands {
   private static final double DEADBAND = 0.1;
-  private static final double ANGLE_KP = 50.0;
-  private static final double ANGLE_KD = 2.0;
+  private static final double ANGLE_KP = 0.5;
+  private static final double ANGLE_KD = 0.1;
   private static final double ANGLE_MAX_VELOCITY = 45.0;
   private static final double ANGLE_MAX_ACCELERATION = 90.0;
   private static final double FF_START_DELAY = 2.0; // Secs
