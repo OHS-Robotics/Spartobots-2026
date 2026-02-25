20ed94c5c91159789c05eb0b5a7e5c234f34e1ff
Liam Barkley <barkley.liam.m@gmail.com>
2026-02-24 23:55:20 -0600

    Fix auto assist scheduling

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -28,6 +28,7 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -507,7 +508,7 @@ public class RobotContainer {
               }
             });
     activeAutoAssistCommand = trackedCommand[0];
-    activeAutoAssistCommand.schedule();
+    CommandScheduler.getInstance().schedule(activeAutoAssistCommand);
   }
 
   private void cancelAutoAssist() {
