c7d936b7cd506c2dfdd818fb2593b2ceb5e98836
Coda <barkley.liam.m@gmail.com>
2026-03-05 21:05:43 -0600

    build leftovers

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "Spartobots-2026";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 184;
+  public static final String GIT_SHA = "71dcea341f2b66378fda23683f40ff37148095d3";
+  public static final String GIT_DATE = "2026-03-03 21:07:35 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2026-03-03 21:26:04 EST";
+  public static final long BUILD_UNIX_TIME = 1772591164417L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants() {}
+}
